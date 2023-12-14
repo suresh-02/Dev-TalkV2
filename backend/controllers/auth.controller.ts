bd330485416bcb175de5436cd9c9d25f534f9958
suresh-02 <97348927+suresh-02@users.noreply.github.com>
2023-12-14 21:16:33 +0530

    user login controller added and working

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -7,38 +7,9 @@ const userController = {
   //! To register a new user.
   async registerUser(req: Request, res: Response) {
     try {
-      const { username, password, email, image } = req.body; //? getting the  user entering details
-
-      // Check if the user already exists
-      const existingUser = await User.findOne({ where: { email } });
-      if (existingUser) {
-        return res
-          .status(400)
-          .json({ error: "User already exists in database" });
-      }
-
-      // Create a new user
-      const newUser = await User.create({
-        username,
-        password,
-        email,
-        image,
-      });
-
-      return res.status(201).json(newUser);
-    } catch (error) {
-      console.error("Error registering user:", error);
-      return res.status(500).json({ error: "Failed to register user" });
-    }
-  },
-
-  //! To login a new user
-
-  async loginUser(req: Request, res: Response) {
-    try {
       const { email, password } = req.body;
 
-      const user = await User.findOne({ where: { email } }); // Check if the user exists in the database
+      const user = await User.findOne({ where: { email } });
 
       if (!user) {
         return res
@@ -46,24 +17,40 @@ const userController = {
           .json({ message: "Invalid username or password" });
       }
 
-      const isValidPassword = await bcrypt.compare(password, user.password); // Compare the provided password with the stored password
+      const isValidPassword = await bcrypt.compare(password, user.password);
+
       if (!isValidPassword) {
         return res
           .status(401)
           .json({ message: "Invalid username or password" });
       }
 
-      // User authentication successful, generate JWT token
       const token = jwt.sign(
         { userId: user.id, email: user.email },
-        "Secret-Key",
+        "Secret-Key", // Replace with your secret key
         { expiresIn: "1h" }
       );
-      return res.status(200).json({ token });
+
+      res.status(200).json({ token });
     } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: "Server Error" });
+      console.error("Error registering user:", error);
+      return res.status(500).json({ error: "Failed to register user" });
     }
+  },
+
+  //! To login a new user
+  async loginUser(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    const userAvaialble = await User.findOne({ where: { email: email } });
+    if (!userAvaialble)
+      return res.status(400).json({ message: "The user does not exist" });
+
+    const isPasswordMatch = bcrypt.compare(password, userAvaialble.password);
+    if (!isPasswordMatch)
+      return res.status(400).json({ message: "The password does not match" });
+
+    return res.status(200).json({ message: "User logged in sucessfully" });
   },
 };
 
